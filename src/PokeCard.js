9eacb71d3ff8bbbe8950a16b3b81e2b0eefe0bf4
Nikhil Bansal <nikbansal1998@gmail.com>
2022-04-11 14:16:56 +0530

    Made resuable PokeCard to display for some Pokemons

---
+++ src/PokeCard.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import "./styles/PokeCard.css"
+
+
+class PokeCard extends Component{
+    render(){
+        const {id, name, type, base_experience} = this.props;
+        const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+        return (
+            <div className="PokeCard">
+                <h2 className="PokeCard-name">{name}</h2>
+                <img className="PokeCard-image" src={imageURL}></img>
+                <p className="PokeCard-type">Type: {type}</p>
+                <p className="PokeCard-exp">Exp: {base_experience}</p>
+            </div>
+        )
+    }
+}
+
+export default PokeCard;
